1bd957eccb120d9f061d662a774d8076b9b8ed2d
Kyle Tong <kyle.simon.tong@gmail.com>
2020-10-19 18:35:01 +0900

    Didnt work, roll back

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
     providerOptions: {
       key: env('AWS_SES_KEY', 'AKIAX3BDJPREDVTHGUFE'),
       secret: env('AWS_SES_SECRET', 'BPX/RUQRoBZ8zFvvz9O2vrv1TvDSFZemLSx9V/O222oz'),
-      amazon: 'https://email-smtp.ap-southeast-2.amazonaws.com:587',
+      amazon: 'https://email-smtp.ap-southeast-2.amazonaws.com',
     },
     settings: {
       defaultFrom: 'hello@entrancedesign.co.nz',
