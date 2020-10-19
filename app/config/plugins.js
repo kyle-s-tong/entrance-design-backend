1efd42363f69842639de4ffcf94e8820963c210d
Kyle Tong <kyle.simon.tong@gmail.com>
2020-10-20 07:10:38 +0900

    Move to other implementattion of plugin

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -12,5 +12,16 @@ module.exports = ({ env }) => ({
       defaultReplyTo: 'hello@entrancedesign.co.nz',
     },
   },
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_API_KEY'),
+      secretAccessKey: env('AWS_SECRET_KEY'),
+      region: "ap-southeast-2",
+      params: {
+        Bucket: env('AWS_IMAGE_BUCKET'),
+      },
+    },
+  },
   // ...
 });
