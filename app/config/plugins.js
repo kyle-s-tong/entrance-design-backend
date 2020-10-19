62a0523d8257799d34859a64b1151351f266fb5d
Kyle Tong <kyle.simon.tong@gmail.com>
2020-10-19 18:30:25 +0900

    Try using port

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
   email: {
     provider: 'amazon-ses',
     providerOptions: {
-      key: env('AWS_SES_KEY'),
-      secret: env('AWS_SES_SECRET'),
-      amazon: 'https://email-smtp.ap-southeast-2.amazonaws.com',
+      key: env('AWS_SES_KEY', 'AKIAX3BDJPREDVTHGUFE'),
+      secret: env('AWS_SES_SECRET', 'BPX/RUQRoBZ8zFvvz9O2vrv1TvDSFZemLSx9V/O222oz'),
+      amazon: 'https://email-smtp.ap-southeast-2.amazonaws.com:587',
     },
     settings: {
       defaultFrom: 'hello@entrancedesign.co.nz',
