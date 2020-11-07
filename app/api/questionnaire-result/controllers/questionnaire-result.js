f548731aa8629e13db772a114d51252ddd82d711
Kyle Tong <kyle.simon.tong@gmail.com>
2020-11-07 11:01:15 +0900

    Update user model to have the questionnaire results and create an endpoint to store the data and email it. Still need to check if the user exists first

--- app/api/questionnaire-result/controllers/questionnaire-result.js
+++ app/api/questionnaire-result/controllers/questionnaire-result.js
@@ -1,8 +1,32 @@
 'use strict';
 
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    const user = {
+      ...ctx.request.body,
+      provider: 'local'
+    }
+    const data = await strapi.plugins['users-permissions'].services.user.add(user);
+
+    await strapi.plugins['email'].services.email.send({
+      to: 'kyle.simon.tong@gmail.com',
+      from: 'hello@entrancedesign.co.nz',
+      subject: 'A new user completed the design quiz',
+      text: `
+        A new user completed the design quiz. Their email address is ${data.email}
+
+        Their quiz result was:
+        ${data.questionnaireResult}
+      `,
+    });
+
+    return 'strapi';
+  },
+};
