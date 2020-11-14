7d02f0c2531281b4717056ab722e49804e78c226
Kyle Tong <kyle.simon.tong@gmail.com>
2020-11-14 20:58:56 +0900

    Revert Revert

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -1,25 +1,4 @@
 module.exports = ({ env }) => ({
-  email: {
-    provider: 'amazon-ses',
-    providerOptions: {
-      key: env('AWS_API_KEY'),
-      secret: env('AWS_SECRET_KEY'),
-      amazon: 'https://email.ap-southeast-2.amazonaws.com',
-    },
-    settings: {
-      defaultFrom: 'hello@entrancedesign.co.nz',
-      defaultReplyTo: 'hello@entrancedesign.co.nz',
-    },
-  },
-  upload: {
-    provider: 'aws-s3',
-    providerOptions: {
-      accessKeyId: env('AWS_API_KEY'),
-      secretAccessKey: env('AWS_SECRET_KEY'),
-      region: "ap-southeast-2",
-      params: {
-        Bucket: env('AWS_IMAGE_BUCKET'),
-      },
-    },
-  },
+
+  // ...
 });
